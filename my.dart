b9f457879acc5cb62f1fdc25b59f89a688f621a9
IsraZafar <161151945+IsraZafar@users.noreply.github.com>
2024-05-12 13:57:48 +0500

    Add files via upload

---
+++ my.dart
@@ -0,0 +1,46 @@
+ void main() {
+  List<String> StudentName =["fatima"];
+  List <int> Studentage =[12];
+  List<double> StudentPercentage=[78.4];
+  
+  Map studentdata = {"name":"fatima","age":12,"percentage":78.4};
+  print (["name is fatima"]);
+  print ( ["age is 12"]);
+  print( ["percentage is 78.4"]);
+
+
+
+
+var name = "herry";
+print (name);
+int eng =78;
+int urdu = 45;
+int math = 62;
+int totalmarks=100;
+int obtainmarks=(eng +urdu+math);
+  print("Obtain Marks is ""$obtainmarks");
+ print("Total Marks is" "$totalmarks");
+double per = totalmarks/obtainmarks*100;
+ print ("Percentage is " "$per");
+
+
+
+
+  num percentage = 79.0;
+  if (percentage>=50){
+    print("pass ");
+    
+  }
+else{
+print ("fail");
+
+}
+}         
+         
+
+
+
+
+
+
+
